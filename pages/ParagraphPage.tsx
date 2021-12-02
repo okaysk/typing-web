b5f29f5f80717d69ac189ab3a839bf37ea71ead3
hashpower-defi <67887045+okaysk@users.noreply.github.com>
2021-12-02 11:34:20 +0900

    input 맞으면 초록색까지 구현

--- pages/ParagraphPage.tsx
+++ pages/ParagraphPage.tsx
@@ -2,24 +2,29 @@ import { useRef, useState } from 'react'
 
 let someWords: any
 function ParagraphPage() {
-    const [paragraph, setParagraph] = useState('')
     const [words, setWords] = useState([''])
+    const [userInput, setUserInput] = useState('')
+    const [answerIndex, setAnswerIndex] = useState(0)
+    const [correct, setCorrect] = useState([false, ''])
+    const inputRef = useRef<any>()
 
-    // const request = new Request('http://metaphorpsum.com/paragraphs/1/4')
+    const answer = [
+        words.map((word, index) => (
+            <span className={`${correct[index]} text-3xl leading-loose`} key={index}>
+                {word}
+            </span>
+        )),
+    ]
 
     async function getParagraph() {
-        console.log(`getParagraph() called`)
-        const config = {
-            headers: {
-                Accept: 'application/json',
-            },
-        }
+        console.log(`getParagraph()`)
 
-        await fetch('http://metaphorpsum.com/paragraphs/1/1', config)
+        // (Re)set - answerIndex, correct, userInput
+
+        await fetch('http://metaphorpsum.com/paragraphs/1/1')
             .then(response => response.blob())
             .then(data => data.text())
             .then(text => {
-                setParagraph(text)
                 const word_raw = text.split('')
                 console.log(`text : ${text}`)
                 console.log(`word_raw : ${word_raw}`)
@@ -33,32 +38,40 @@ function ParagraphPage() {
             })
     }
 
-    console.log(`Rendering~~~~~~~`)
+    const keyboardFocus = () => {
+        inputRef.current?.focus()
+    }
 
-    console.log(`Rendering- wordState: ${words[0]} ${words[1]} ${words[2]}`)
-
-    const arr = ['1', '2']
-    const res = [arr]
-    console.log(`\t\t typeof [arr]  : ${typeof res}`)
-    console.log(`\t\t typeof [...arr]  : ${typeof [...arr]}`)
-    console.log(`\t\t typeof {...arr]  : ${typeof { ...arr }}`)
-
-    const sample = [
-        words.map((word, index) => (
-            <span className="text-3xl leading-loose" key={index}>
-                {word}
-            </span>
-        )),
-    ]
-
-    const [userInput, setUserInput] = useState('')
-    const [index, setIndex] = useState(0)
-    const [correct, setCorrect] = useState([false, ''])
-    const inputRef = useRef<any>([])
+    keyboardFocus()
 
     const inputCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
         // Input
         const inputValue = e.target.value
+        const lastChar = inputValue.charAt(inputValue.length - 1)
+        console.log(`input: ${inputValue}`)
+        console.log(`last: ${lastChar}`)
+        console.log(`userInput State : ${userInput}`)
+
+        setUserInput(inputValue)
+
+        if (lastChar == words[answerIndex]) {
+            console.log(`input is : ${inputValue}`)
+            setCorrect({ ...correct, [answerIndex]: 'correct' })
+            setAnswerIndex(answerIndex + 1)
+        } else {
+            // setUserInput(inputValue)
+            setCorrect({ ...correct, [answerIndex]: 'incorrect' })
+            setTimeout(() => {
+                // setUserInput('')
+                setCorrect({ ...correct, [answerIndex]: '' })
+            }, 900)
+        }
+    }
+
+    const keyDown = (e: any) => {
+        if (e.keyCode === 8) {
+            setAnswerIndex(answerIndex - 1)
+        }
     }
 
     return (
@@ -67,13 +80,16 @@ function ParagraphPage() {
                 <div>Test</div>
             </div>
             <div className="w-2/3 relative">
-                {sample}
+                {answer}
                 <input
                     className="paragraph-input text-3xl w-full text-left align-text-top border-blue-800 text-blue-400 leading-loose focus:outline-none"
                     type="text"
                     value={userInput}
-                    onChange={e => setUserInput(e.target.value)}
+                    onChange={e => inputCheck(e)}
+                    onKeyDown={e => keyDown(e)}
+                    ref={el => (inputRef.current = el)}
                 ></input>
+
                 {/* <textarea
                     // value={userInput}
                     className="paragraph-input text-4xl w-full h-full text-left align-text-top border-2 border-blue-200 absolute leading-loose opacity-80"
@@ -83,7 +99,9 @@ function ParagraphPage() {
                 {/* <div className="paragraph-sample text-4xl text-gray-400 leading-loose">{paragraph}</div> */}
             </div>
             <img src="https://i.gifer.com/1uUh.gif" className="rounded-xl h-40 w-40" />
-            <button onClick={getParagraph}>Get Paragraph!</button>
+            <button className="border-2 bg-gray-200 py-3 px-6 rounded-lg mt-4" onClick={getParagraph}>
+                Get Paragraph!
+            </button>
         </div>
     )
 }
